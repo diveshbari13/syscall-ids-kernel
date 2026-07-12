2d27fe24e102cdc4291c28a50fa447873e50f346
diveshbari13 <diveshbari13@gmail.com>
2026-07-12 07:38:57 +0000

    Step 1: kernel boots via GRUB multiboot, prints vga

---
+++ kernel/kmai.c
@@ -0,0 +1,9 @@
+void kmain(void) {
+    char *vga = (char*)0xB8000;
+    const char *msg = "Kernel booted successfully.";
+    for (int i = 0; msg[i]; i++) {
+        vga[i*2] = msg[i];
+        vga[i*2+1] = 0x0F;
+    }
+    while (1) {}
+}
